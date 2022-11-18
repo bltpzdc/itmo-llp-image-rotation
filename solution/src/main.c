a182d57f4b3176c8d02b9013623646892c8c7bdf
bltpzdc <oblyashevsky@yandex.ru>
2022-11-19 01:46:08 +0300

    Мейн приведен в порядок. Косметические правки

--- solution/src/main.c
+++ solution/src/main.c
@@ -19,10 +19,9 @@ int main(int argc, char** argv){
         fprintf(stderr, "Can not open input file.");
         return 1;
     }
-
     fprintf(stdout, "Input file opened.");
-    struct image old_image = {0};
 
+    struct image old_image = {0};
     if (read_bmp_file(input, &old_image) != READ_OK){
         fprintf(stderr, "Can not read input file.");
         free(old_image.data);
@@ -38,28 +37,27 @@ int main(int argc, char** argv){
     }
     fprintf(stdout, "Input file closed.");
 
-
     FILE* output = fopen(argv[2], "wb");
     if (!output){
         fprintf(stderr, "Can not open input file.");
+        free(old_image.data);
         return 1;
     }
 
     struct image new_image = rotate(&old_image);
     free(old_image.data);
-
     if (write_bmp_file(output, &new_image) != WRITE_OK){
         fprintf(stderr, "Can not write into output file.");
         free(new_image.data);
         fclose(output);
         return 1;
     }
+    fprintf(stdout, "New image written to output file.");
+    free(new_image.data);
 
     if (fclose(output) != 0 ){
-        fprintf(stderr, "cant close");
-        free(new_image.data);
+        fprintf(stderr, "Can not close output file.");
         return 1;
     }
-    free(new_image.data);
     return 0;
 }
